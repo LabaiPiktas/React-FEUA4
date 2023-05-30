111ffb136c7fa01616978c6388f3779ddf30cdd0
LabaiPiktas <121252533+LabaiPiktas@users.noreply.github.com>
2023-05-30 22:04:43 +0300

    dadėtas tikrinimas

--- 9_paskaita_pratimas/src/components/User.jsx
+++ 9_paskaita_pratimas/src/components/User.jsx
@@ -2,6 +2,10 @@ import PropTypes from "prop-types";
 import "../App.css";
 
 const User = ({ name, last_name, avatar }) => {
+  if (!name || !last_name || !avatar) {
+    return null;
+  }
+
   return (
     <div className="post">
       <h2>{name}</h2>
