7791fa22cacb9741d6549148d816fa363d345cd9
LabaiPiktas <labailabaipiktas.com>
2023-06-07 18:24:24 +0300

    paskutinis

--- 10_paskaita_2pratimas/src/pages/Main.jsx
+++ 10_paskaita_2pratimas/src/pages/Main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Main.css";
-import HeroImage from "./img/logo.jpg";
+import HeroImage from "./img/rhondak.jpg";
 
 const Main = () => {
   return (
     <div>
-      <h1>Main Page</h1>
       <div className="hero">
         <img src={HeroImage} alt="Hero" />
+        <h1 className="hero-text">Main Page</h1>
       </div>
     </div>
   );
